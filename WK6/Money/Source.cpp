d3c10f0cdea28971fb44848b294ed246aa5897c1
Jordan D <Jordod@users.noreply.github.com>
2018-10-27 16:00:11 +0100

    Update Source.cpp

--- WK6/Money/Source.cpp
+++ WK6/Money/Source.cpp
@@ -1,20 +1,38 @@
 #include "Money.h"
-#include <string>
 
 using namespace std;
 
 int main()
-{	
-	const string CURRENCY_A = "EURO";
-	Money a(CURRENCY_A, 100, 45);
-	Money b(CURRENCY_A, 10, 45);
+{
+    const string CURRENCY_A = "EURO";
 
-	Money c = b * 2;
-	cout << c.getMajor() << ' ' << c.getMinor() << endl;
+    Money a(CURRENCY_A, 100, 90);
+    Money b(CURRENCY_A, 10, 45);
 
-	c = a / 2;
-	cout << c.getMajor() << ' ' << c.getMinor() << endl;
+    cout << "After Initialisation, using stream op to print" << endl;
+    cout << "A: " << a << endl;
+    cout << "B: " << b << endl;
 
-	system("pause");
-	return 0;
+    cout << endl << "c = a - b" << endl;
+    Money c = a - b;
+    cout << "C: " << c << endl;
+
+    cout << endl << "c = c * 2" << endl;
+    c = c * 2;
+    cout << "C: " << c << endl;
+
+    cout << endl << "c = c / 4" << endl;
+    c = c / 4;
+    cout << "C: " << c << endl;
+
+    cout << endl << "Testing comparision ops" << endl;
+    cout << "a == b " << (a == b ? "true" : "false") << endl;
+    cout << "a == a " << (a == a ? "true" : "false") << endl;
+    cout << "a < b " << (a < b ? "true" : "false") << endl;
+    cout << "b < a " << (b < a ? "true" : "false") << endl;
+    cout << "a > b " << (a > b ? "true" : "false") << endl;
+    cout << "b > a " << (b > a ? "true" : "false") << endl;
+
+    system("pause");
+    return 0;
 }
