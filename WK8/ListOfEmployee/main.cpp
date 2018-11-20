cfdc2f91548117d44102496906f1bab7ad89aa00
Jordan D <Jordod@users.noreply.github.com>
2018-11-20 20:14:21 +0000

    Update main.cpp

--- WK8/ListOfEmployee/main.cpp
+++ WK8/ListOfEmployee/main.cpp
@@ -5,32 +5,38 @@ using namespace std;
 
 int main() {
 	ListOfEmployee list;
-
-	for (int i = 0; i < 4; i++) {
-		list.push_front(string("Front") + to_string(i), (i + 1) * 100);
-	}
 	
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < 3; i++) {
 		list.push_back(string("Back") + to_string(i), (i + 1) * 100);
 	}
+	
+	for (int i = 0; i < 2; i++) {
+		list.push_front(string("Front") + to_string(i), (i + 1) * 100);
+	}
+
 
 	cout << "After Init:" << endl;
 	cout << list;
-
-	cout << "Deleted: " << list.deleteAtFront() << endl;
+	cout << "Deleted At Front: " << list.deleteAtFront() << endl;
+	cout << "Deleted At Front: " << list.deleteAtFront() << endl;
 	cout << list;
 	cout << "Found : " << list.getSalary("Front0") << " for Employee Front0" << endl;
 
 	ListOfEmployee list2 = list;
 
-	list2.deleteAtEnd();
-	list2.deleteAtEnd();
+	list2.push_back("List2", 100);
+	list2.push_back("List2", 100);
 
-	cout << "List after copy constructor and 2 deletes" << endl;
+	cout << "List 2: (After 2 push_back calls)" << endl;
 	cout << list2;
 	cout << "List 1:" << endl;
 	cout << list;
 	
-	system("pause");
+	cout << "Deleted At End: " << list.deleteAtEnd() << endl;
+	cout << list;
+	
+	list.insert(1, Employee("Insert", 100));
+	cout << "Insert at pos = 1 " << endl << list;
+	
 	return 0;
 }
